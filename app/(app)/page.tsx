edf039c3cbd299ade2666faa48465a4bfcd485be
Prateekk02 <prateek.kumar2002@gmail.com>
2025-04-17 22:14:22 +0530

    Added dark/light mode, prisma and shadcn

---
+++ app/(app)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ThemeToggle } from '@/components/ui/ThemeToggle'
+
+export default function Hero() {
+  return (
+    <div>Hero
+
+      <ThemeToggle />
+    </div>
+  )
+}
