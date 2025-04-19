13df772f52c4c7bbd07e6de000fc5858221f90e8
Prateekk02 <prateek.kumar2002@gmail.com>
2025-04-20 04:00:29 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "../lib/providers/ThemeProviderWrapper";
 import "./globals.css";
+import { NavigationBar } from "@/components/NavigationBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            
+            <NavigationBar />
             {children}
           </ThemeProvider>
       </body>
